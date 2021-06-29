6f64714de0e31c5e892dffcdb8a2140475202531
songa1 <songachillethe1her@gmail.com>
2021-06-30 00:13:28 +0200

    Edited the link in js files

--- js/posts.js
+++ js/posts.js
@@ -35,6 +35,6 @@ function show(post) {
 let arrow = document.getElementById('arrow');
 
 arrow.addEventListener('click',()=>{
-    location.href(`index.html`)
+    location.assign(`index.html`)
     localStorage.removeItem('editor')
 })
