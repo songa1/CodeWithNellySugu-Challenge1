ac91076c7348fefca3946821ecb9550fc54f059d
songa1 <songachillethe1her@gmail.com>
2021-06-29 23:47:01 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const api_url = "https://jsonplaceholder.typicode.com/users";
+
+async function getapi(url) {
+    
+    const response = await fetch(url);
+    let data = await response.json();
+    data.forEach(user => {
+        show(user);
+    })
+    
+}
+
+getapi(api_url)
+
+function show(user) {
+    let dataCont = document.querySelector('.user-cont');
+    let div = document.createElement('div');
+    div.setAttribute('class', 'user')
+    let name = document.createElement('p');
+    let email = document.createElement('p');
+    let btn = document.createElement('button')
+
+    div.appendChild(name);
+    div.appendChild(email);
+    div.appendChild(btn);
+    dataCont.appendChild(div);
+    name.textContent = `Name: ${user.name}`
+    email.textContent = `Email: ${user.email}`
+    btn.textContent = `Get User’s Posts`
+
+    btn.addEventListener('click', () => {
+        location.assign(`/posts.html#${user.id}`);
+        localStorage.setItem('editor', user.name);
+    })
+    
+}
---
+++ js/posts.js
@@ -0,0 +1,40 @@
+let id = location.hash.slice(1);
+let editor = localStorage.getItem('editor');
+
+let topTitle = document.getElementById('topTitle');
+topTitle.innerHTML = `Posts by ${editor}`
+
+const api_url = `https://jsonplaceholder.typicode.com/posts?userId=${id}`;
+
+async function getapi(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    data.forEach(post => {
+        show(post)
+    })
+}
+
+getapi(api_url)
+
+function show(post) {
+    let dataCont = document.querySelector('.post-cont');
+    let div = document.createElement('div');
+    div.setAttribute('class', 'user')
+    let title = document.createElement('h3');
+    title.setAttribute('class', 'title');
+    let body = document.createElement('p');
+
+    div.appendChild(title);
+    div.appendChild(body);
+    dataCont.appendChild(div);
+
+    title.textContent = `${post.title}`
+    body.textContent = `${post.body}`
+}
+
+let arrow = document.getElementById('arrow');
+
+arrow.addEventListener('click',()=>{
+    location.assign(`index.html`)
+    localStorage.removeItem('editor')
+})
